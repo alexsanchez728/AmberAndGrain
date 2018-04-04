3fcbe2b6b2a111ee8e44d31efc104923d5dba435
Alex Sanchez <alesanchez728@gmail.com>
2018-04-03 20:29:31 -0500

    added create recipe

--- AmberAndGrain/Controllers/RecipesController.cs
+++ AmberAndGrain/Controllers/RecipesController.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AmberAndGrain.Models;
+using AmberAndGrain.Services;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
 namespace AmberAndGrain.Controllers
 {
+    [RoutePrefix("api/recipes")]
     public class RecipesController : ApiController
     {
+        [Route, HttpPost]
+        public HttpResponseMessage AddRecipe(RecipeDto recipe)
+        {
+            var recipeRepository = new RecipeRepository();
+            var result = recipeRepository.Create(recipe);
+
+            
+           return ((result) 
+                ? Request.CreateResponse(HttpStatusCode.Created) 
+                : Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Could not create recipe. Please try again."));
+        }
     }
 }
---
+++ AmberAndGrain/Services/RecipeRepository.cs
@@ -0,0 +1,33 @@
+﻿using AmberAndGrain.Models;
+using Dapper;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AmberAndGrain.Services
+{
+    public class RecipeRepository
+    {
+        public bool Create(RecipeDto recipe)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["AmberAndGrain"].ConnectionString))
+            {
+                db.Open();
+                var numberCreated = db.Execute(@"INSERT INTO [dbo].[Recipes]
+                           ([Name]
+                           ,[PercentWheat]
+                           ,[PercentCorn]
+                           ,[BarrelAge]
+                           ,[BarrelMaterial]
+                           ,[Creator])
+                     VALUES
+                           (@Name
+                           ,@PercentWheat
+                           ,@PercentCorn
+                           ,@BarrelAge
+                           ,@BarrelMaterial
+                           ,@Creator)", recipe);
+                return numberCreated == 1;
+            }
+        }
+    }
+}
